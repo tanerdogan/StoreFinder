7b097d0864d60de064445060b7bd9dfb14c41eaa
Taner DOGAN <codersneferu@gmail.com>
2012-10-31 16:47:29 +0200

    Seperate JS

--- index.php
+++ index.php
@@ -13,76 +13,23 @@
         <meta http-equiv="Content-Language" content="en" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Store Finder</title>
-    <script type="text/javascript" src="js/jquery-1.8.0.min.js"></script>
+    <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>-->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+
     <script src="http://j.maxmind.com/app/geoip.js"></script>
     <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
     <script type="text/javascript" src="js/markerclusterer.js"></script>
+    <script type="text/javascript" src="js/map.Maker.js"></script>
+
+    <script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+    <script type="text/javascript" src="js/jScrollPane.js"></script>
+
     <script type="text/javascript" src="data.json"></script>
-
-
-
-    <link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="css/storefinder.css" />
+    <link rel="stylesheet" type="text/css" media="all" href="css/jScrollPane.css" />
 
-
-
-
-    <script type="text/javascript">
-
-        $(document).ready(function(){
-            $(".fade").mouseenter(function () {
-                $(this).fadeTo( "normal", 1);
-            }).mouseleave(function(){
-                $(this).fadeTo( "slow", 0.6);
-            });
-        });
-
-
-        function initialize() {
-            var center = new google.maps.LatLng(37.4419, -122.1419);
-
-            var infowindow = new google.maps.InfoWindow();
-
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 4,
-                center: center,
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                panControl: false,
-                zoomControl: true,
-                zoomControlOptions: {
-                    style: google.maps.ZoomControlStyle.LARGE,
-                    position: google.maps.ControlPosition.LEFT_CENTER
-                },
-                scaleControl: false,
-                mapTypeControl: false
-            });
-
-            var markers = [];
-            for (var i = 0; i < 100; i++) {
-                var dataPhoto = data.photos[i];
-                var latLng = new google.maps.LatLng(dataPhoto.latitude,
-                dataPhoto.longitude);
-                var marker = new google.maps.Marker({
-                    position: latLng
-                });
-
-                google.maps.event.addListener(marker, 'click', function() {
-                    infowindow.setContent('test<br />test,Texas');
-                    infowindow.open(map, marker);
-                    console.log(i);
-                });
-
-                markers.push(marker);
-            }
-            var markerCluster = new MarkerClusterer(map, markers);
-
-
-        }
-
-
-
-        google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
+    <script type="text/javascript" src="js/visual.js"></script>
+    <script type="text/javascript" src="js/map-init.js"></script>
 
 </head>
 <body>
@@ -90,11 +37,6 @@
 <div class="layout-center">
     <div id="map"></div>
 </div>
-
-
-
-
-
 
 <div class="layout-north fade">
     <div class="title">
@@ -105,7 +47,12 @@
     <a href="#" class="header-button">BUTTON</a>
     <a href="#" class="header-button">BUTTON</a>
 </div>
-<div class="layout-side fade">Side Bar</div>
+<div class="layout-side fade">
+    <h1 class="h1-title">ÇEVREDEKİLER</h1> <br />
+    <div id="sidebar" class="sidebar_item">
+        
+    </div>
+</div>
 
 
 </body>
